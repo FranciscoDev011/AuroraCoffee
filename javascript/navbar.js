18e15a8e1717868cee20af96d15b74e37f196837
FranciscoDev011 <franciscodejesusalves99@gmail.com>
2026-06-18 19:08:40 -0300

    Add favicon images and implement navbar scroll effect with intersection observer - Added an intersection observer to reveal elements with the class 'hidden' when they come into view. - Created a new JavaScript file (script.js) for the navbar and observer functionality. - And new changes.

--- javascript/navbar.js
+++
@@ -1,9 +0,0 @@
-const navbar = document.querySelector("header");
-
-window.addEventListener("scroll", () => {
-    if (window.scrollY >= 20) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
-});
---
+++ javascript/script.js
@@ -0,0 +1,24 @@
+/* Navbar Scroll */
+const navbar = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY >= 20) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
+
+
+/* Observer */
+const myObserver = new IntersectionObserver( (entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    })
+    }, {threshold: 0.4 }
+)  
+
+const animated = document.querySelectorAll('.hidden');
+animated.forEach((element) => myObserver.observe(element));
